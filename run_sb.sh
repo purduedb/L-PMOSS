4f004ef34cac4b222c0f73a37c94d3ec2f6a0fbd
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-26 07:19:55 -0400

    clean learned

--- run_sb.sh
+++ run_sb.sh
@@ -9,3 +9,16 @@ python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/
 python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 11 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
 python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 13 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
 python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 41 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
+
+
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 11 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 13 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 41 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 11 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 13 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 41 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 11 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 13 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_sb_4s_4n" --mpath "save_models/intel_sb_4s_4n/0/2024-10-24-07-57-37-0.955.pkl" --wl 41 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
