cf3334a1bfad6183c43ec204143c68c3010f4b32
yop <yeasirrayhan.prince@gmail.com>
2024-10-26 14:01:10 +0000

    clean learned

---
+++ run_amd_2s8n.sh
@@ -0,0 +1,24 @@
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 100 --sidx 250 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 100 --sidx 250 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 100 --sidx 250 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 101 --sidx 251 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 101 --sidx 251 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 101 --sidx 251 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 102 --sidx 252 --is_eval_only --rtg 2
+
+=? no masking
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 100 --sidx 253 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 101 --sidx 254 --is_eval_only --rtg 2
+
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 11 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 13 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
+python run_dt_place.py --p "amd_epyc7543_2s_8n" --mpath "save_models/amd_epyc7543_2s_8n/0/2024-10-25-05-08-33-0.941.pkl" --wl 41 --ecfg 102 --sidx 255 --is_eval_only --rtg 2
