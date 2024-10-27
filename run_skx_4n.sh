a8d4bb568bfada6013c3632e853b87755967efbf
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-27 16:33:50 -0400

    clean learned

--- run_skx_4n.sh
+++ run_skx_4n.sh
@@ -10,3 +10,13 @@ python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8
 python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 1 --sidx 207 --is_eval_only --rtg 2
 python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 5 --sidx 208 --is_eval_only --rtg 2
 python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 33 --sidx 209 --is_eval_only --rtg 2
+
+
+
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 100 --sidx 210 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 101 --sidx 211 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 102 --sidx 212 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 43 --sidx 213 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 55  --sidx 214 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 204 --sidx 215 --is_eval_only --rtg 2
+python run_dt_place.py --p "intel_skx_4s_4n" --mpath "save_models/intel_skx_4s_8n/0/2024-10-25-18-14-13-0.992.pkl" --wl 11 --ecfg 207 --sidx 216 --is_eval_only --rtg 2
