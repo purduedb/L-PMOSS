0622e76dc071422ba00a67726b91f500929052f2
yeasir <yeasirrayhan.prince@gmail.com>
2024-10-28 19:33:08 -0400

    clean nvidia learned

--- run_nvidia.sh
+++ run_nvidia.sh
@@ -1,1 +1,8 @@
 python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "None" --wl 11 --ecfg 100 --sidx 200 --rtg 2
+
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 100 --sidx 200 --is_eval_only --rtg 2
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 1 --sidx 201 --is_eval_only --rtg 2
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 3 --sidx 202 --is_eval_only --rtg 2
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 5 --sidx 203 --is_eval_only --rtg 2
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 4 --sidx 204 --is_eval_only --rtg 2
+python run_dt_place.py --p "nvidia_gh_1s_1n" --mpath "save_models/nvidia_gh_1s_1n/0/2024-10-28-17-03-01-0.942.pkl" --wl 11 --ecfg 21 --sidx 205 --is_eval_only --rtg 2
