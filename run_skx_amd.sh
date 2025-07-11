a2f063e9c4d0724730b8b4b0e16b8fd313001f5e
Yeasir <yeasirrayhan.prince@gmail.com>
2025-07-11 18:52:44 +0000

    Clean from amd

---
+++ run_skx_amd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# module load conda
+# conda activate pmoss
+
+# Define model paths as variables
+base_model_wo_nvidia_ibm="/scratch/gilbreth/yrayhan/save_models/intel_sb_4s_4n/0/2025-07-10-17-29-23-0.952.pkl"
+base_model_wo_ibm="/users/yrayhan/works/L-PMOSS/base_models/2025-07-11-12-17-34-0.912.pkl"
+assistant_model_amd_2s8n_v1="/scratch/gilbreth/yrayhan/save_models/amd_epyc7543_2s_8n/0/2025-07-10-22-14-09-0.977.pkl"
+assistant_model_amd_2s8n_v2="/scratch/gilbreth/yrayhan/save_models/amd_epyc7543_2s_8n/0/2025-07-11-00-39-15-0.979.pkl"
+
+# Example: inference for AMD 2s8n with ? model
+# Intel SB: 4s4n []
+# 12: 301, 44: 302, 45: 303, 16: 304, 13: 305
+# []
+for wk in 45; do
+  python run_dt_place.py \
+    --mpath "$base_model_wo_ibm" \
+    --wl $wk \
+    --ecfg 100 \
+    --sidx 303 \
+    --is_eval_only \
+    --rtg 2
+done
+
+
+
+
+# For pre-training 
+# python run_dt_place.py \
+#   --mpath "$base_model_wo_ibm" \
+#   --wl 11 \
+#   --ecfg 100 \
+#   --sidx 200 \
+#   --rtg 2
+
+
