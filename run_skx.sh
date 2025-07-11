0e6ea1f15a416755d5111574c4abbdbffb2f28f0
Yeasir <yeasirrayhan.prince@gmail.com>
2025-07-11 11:02:15 -0400

    Fix nvidia machine connfig

--- run_skx.sh
+++ run_skx.sh
@@ -4,7 +4,8 @@ module load conda
 conda activate pmoss
 
 # Define model paths as variables
-base_model="/scratch/gilbreth/yrayhan/save_models/intel_sb_4s_4n/0/2025-07-10-17-29-23-0.952.pkl"
+base_model_wo_nvidia_ibm="/scratch/gilbreth/yrayhan/save_models/intel_sb_4s_4n/0/2025-07-10-17-29-23-0.952.pkl"
+base_model_wo_ibm="/scratch/gilbreth/yrayhan/save_models/base_models/0/2025-07-11-09-24-15-0.923.pkl"
 assistant_model_amd_2s8n_v1="/scratch/gilbreth/yrayhan/save_models/amd_epyc7543_2s_8n/0/2025-07-10-22-14-09-0.977.pkl"
 assistant_model_amd_2s8n_v2="/scratch/gilbreth/yrayhan/save_models/amd_epyc7543_2s_8n/0/2025-07-11-00-39-15-0.979.pkl"
 
@@ -22,7 +23,7 @@ assistant_model_amd_2s8n_v2="/scratch/gilbreth/yrayhan/save_models/amd_epyc7543_
 
 # For pre-training 
 python run_dt_place.py \
-  --mpath "$base_model" \
+  --mpath "$base_model_wo_ibm" \
   --wl 12 \
   --ecfg 100 \
   --sidx 259 \
