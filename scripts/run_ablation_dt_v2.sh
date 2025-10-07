b144adc06abea78fb5f2e9e049e495c6e323c252
Yeasir <yeasirrayhan.prince@gmail.com>
2025-10-06 20:57:35 -0400

    Shift the shell scripts

--- scripts/run_ablation_dt_v2.sh
+++ scripts/run_ablation_dt_v2.sh
@@ -8,15 +8,12 @@ conda activate pmoss
 default_base_model_wo_ibm="/scratch/gilbreth/yrayhan/save_models/base_models/0/2025-07-11-12-17-34-0.912.pkl"
 current_base_model_wo_ibm="None"
 
-num_layer=(4) # default is 6
-num_heads=(2 4 6)
-embed_size=(16 32 64)
 
+# num_embedding=(32 64 256)
+num_embedding=(64)
 
-for i in "${!num_layer[@]}"; do
-  nl=${num_layer[$i]}
-  # nh=${num_heads[$i]}
-  # es=${embed_size[$i]}
+for i in "${!num_embedding[@]}"; do
+  ne=${num_embedding[$i]}
   python run_dt_place.py \
     --mpath "$current_base_model_wo_ibm" \
     --wl 11 \
@@ -24,7 +21,7 @@ for i in "${!num_layer[@]}"; do
     --sidx 200 \
     --rtg 2 \
     --ablation_study \
-    --ablation_param num_layer \
-    --n_layer "$nl"
+    --ablation_param num_embedding \
+    --n_embd "$ne"
 done
 
